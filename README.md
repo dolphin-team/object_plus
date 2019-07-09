c413a933b64e60261ecac8e9122706cc5ff08ccb
fencer-yd <982039620@qq.com>
2019-07-09 14:35:23 +0800

    project init

---
+++ README.md
@@ -0,0 +1,51 @@
+# object_plus
+
+# object_plus
+
+# Usage
+```
+npm install @sausage_team/object_plus
+or
+yarn add @sausage_team/object_plus
+```
+
+# Configuration
+
+### isArray()
+
+```
+const a = {}
+a.isArray() //false
+const b = []
+b.isArray() //true
+```
+
+### arrayLength()
+```
+const a = {}
+a.arrayLength() // -1
+const b = []
+b.arrayLength() // 0
+const c = [1]
+b.arrayLength() // 1
+```
+
+### objectLength()
+```
+const a = []
+a.objectLength() //-1
+const b = {}
+b.objectLength() //0
+const c = {a: 1}
+b.objectLength() //1
+```
+
+### getter()
+```
+const a = {
+    b: {
+        c: 1
+    }
+}
+a.getter('b.c') // 1
+```
---
+++ build/object_tool.js
@@ -0,0 +1,50 @@
+Object.defineProperty(Object.prototype, 'isArray', {
+    value: function () {
+        return typeof this === 'object' && Object.prototype.toString.call(this) === '[object Array]';
+    }
+});
+Object.defineProperty(Object.prototype, 'isEmpytArray', {
+    value: function () {
+        return typeof this === 'object' &&
+            Object.prototype.toString.call(this) === '[object Array]' &&
+            this.length > 0;
+    }
+});
+Object.defineProperty(Object.prototype, 'notBlock', {
+    value: function () {
+        return this && ((typeof this === 'object' &&
+            Object.prototype.toString.call(this) === '[object Array]' &&
+            this.length > 0) || (typeof this === 'object' &&
+            Object.prototype.toString.call(this) !== '[object Array]' &&
+            this.isEmpytArray(Object.keys(this))));
+    }
+});
+Object.defineProperty(Object.prototype, 'getter', {
+    value: function (path, options) {
+        if (options === void 0) { options = {}; }
+        var REG_KEY = /\[(['"_a-zA-Z0-9]*)\]|\./gi;
+        var pathArr = path.split(REG_KEY).filter(function (item) { return !!item; });
+        var result = pathArr.reduce(function (result, currentPath, currentIndex) {
+            if (!result.errorPath) {
+                var key = currentPath.replace(/[\'\"]/gi, '');
+                result.value = result.value[key];
+                if (currentIndex !== pathArr.length - 1) {
+                    var currentValueType = Object.prototype.toString.call(result.value);
+                    if (/String|Number|Boolean|Null|Undefined/.test(currentValueType)) {
+                        result.errorPath = currentPath;
+                    }
+                }
+            }
+            return result;
+        }, {
+            value: this,
+            errorPath: null
+        });
+        if (options.errorCallback &&
+            Object.prototype.toString.call(options.errorCallback) === '[object Function]') {
+            options.errorCallback(result.errorPath, pathArr);
+        }
+        return result.value;
+    }
+});
+//# sourceMappingURL=object_tool.js.map
